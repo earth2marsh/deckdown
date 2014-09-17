04e6ea366283c1dcbef18fb6bfaafcd6b880eaf5
earth2marsh <marsh@earth2marsh.com>
2014-09-17 16:26:24 -0700

    tweaked the path and rendered deck

--- api/controllers/reveal.js
+++ api/controllers/reveal.js
@@ -4,6 +4,19 @@ var request = require('request');
 var async = require('async');
 var GitHubApi = require("github");
 
+var fs = require("fs")
+  , ejs = require('ejs')
+  , lessMiddleware = require('less-middleware')
+  , marked = require('marked')
+  , extend = require('node.extend')
+  , path = require('path')
+//  , express = require('express')
+//  , request = require('request')
+  , querystring = require('querystring')
+  , bodyParser = require('body-parser')
+  , validator = require('validator');
+//  , app = express();
+
 var github = new GitHubApi({
     // required
     version: "3.0.0",
@@ -22,15 +35,22 @@ module.exports = {
 }
 
 function reveal(req, res) {
-  var gist = req.swagger.params.gist.value;
-  var url = "https://api.github.com/gists/"+gist;
+  var gistid = req.swagger.params.gistid.value;
+  // var url = "https://api.github.com/gists/"+gist;
   //console.log('Executing request: '+url);
   github.gists.get({
-      id: gist
+      id: gistid
     },
     function(err, response) {
 //      JSON.stringify(response);
-        res.send(response.files["example.md"].content);
+      for (var key in response.files) {
+        if (response.files[key].language === "Markdown") {
+			res.send(ejs.render(template.deck, {
+			  content: slides(response.files[key].content)
+			}));
+			break;
+        }
+      }
     }
   );
   
@@ -40,12 +60,61 @@ function reveal(req, res) {
 //  res.json(html);
 }
 
-function getWeatherByCity(req, res) {
-	var city = req.swagger.params.city.value;
-	var url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial";
-	console.log('Executing request: '+url);
-	request.get(url).pipe(res);
-};
-
 function getGistRawMarkdown(id) {
 };
+
+marked.setOptions({
+  highlight: function (code) {
+    return require('highlight.js').highlightAuto(code).value;
+  }
+});
+
+//load the templates
+var template = {
+  deck: fs.readFileSync(process.cwd() + "/api/templates/deck.html", "utf8"), 
+  slides: fs.readFileSync(process.cwd() + "/api/templates/masters/default.html", "utf8")
+};
+
+function slides(body){
+  var rawSlides = mdToHtmlArray(body);
+  //combine
+  var slides = rawSlides.map(function(slide, i){
+    return ejs.render(template.slides, {
+      content: slide.content
+    });
+  });
+  return slides.join("");
+}
+
+function mdToHtmlArray(markdown){
+    var html = marked(markdown);
+    //Array of values we need to prepend after the split
+    var headers = html.match(/(<h[1-6])|(<hr>)/g); 
+    
+    //leave a marker for splitting
+    html = html.replace(/(<h[1-6])|(<hr>)/g, '<========/*slide*/=========>'); 
+    var slides = html.split("<========/*slide*/=========>"); 
+    
+    //element 0 is whitespace. artifact of split method
+    slides.shift(); 
+    
+    slides = slides.map(function(s, i){
+      //add proper header number back after it was lost in the replace
+      var result = {};
+      if(headers[i] == "<hr>"){
+        result = {
+          'level': 'text',
+          'content': s
+        };
+      }
+      else{
+        result = {
+          'level': headers[i].slice(1),
+          'content': headers[i] + s
+        };
+      }      
+      return result; 
+    });
+    
+    return slides;
+  }
